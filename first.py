e5be5324038f9720e3034b14549e9762cf71d83f
sarangspokharel07-afk <sarangspokharel07@gmail.com>
2026-07-28 12:19:37 +0545

    first commit

---
+++ first.py
@@ -0,0 +1,12 @@
+a=20
+b=10
+c1=a+b
+print("The sum of a and b is:", c1)
+c2=a-b
+print("The difference of a and b is:", c2)
+c3=a*b
+print("The product of a and b is:", c3)
+c4=a/b
+print("The division of a and b is:", c4)
+c5=a%b
+print("The modulus of a and b is:", c5)
