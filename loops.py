b3d6766c09984ca5dd56b28df2293c42c71d6496
sarangspokharel07-afk <sarangspokharel07@gmail.com>
2026-08-10 12:31:49 +0545

    multiplication table

--- loops.py
+++
@@ -1,3 +0,0 @@
-i=[1,2,3,4,5,6,"happy"]
-for a in range(len(i)):
-    print(i[a])
---
+++ multiplication_table.py
@@ -0,0 +1,4 @@
+#print the multiplication table of a given number using a for loop
+num=int(input("enter a number"))
+for i in range(1,11):
+    print(num,"*",i,"=",num*i)
