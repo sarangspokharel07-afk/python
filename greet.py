20f458dbe74d5470966785e226f08cba00abfbf7
sarangspokharel07-afk <sarangspokharel07@gmail.com>
2026-08-10 12:36:14 +0545

    greet names

---
+++ greet.py
@@ -0,0 +1,5 @@
+#greet everyone in a list whose name start with S.
+names = ["Alex", "Bob", "Happy", "Shyam", "Sunil", "Henry"]
+for name in names:
+    if name.startswith("S"):
+        print(f"Hello, {name}!")g
