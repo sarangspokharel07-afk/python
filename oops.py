75432c0d4ce7c7f237cb13978610170c0ddb886a
sarangspokharel07-afk <sarangspokharel07@gmail.com>
2026-08-24 12:27:37 +0545

    object class

---
+++ oops.py
@@ -0,0 +1,14 @@
+class Employee:
+    company="Google"
+    age=""
+    address=""
+    
+harry=Employee()
+harry.company
+harry.age="32"
+harry.address="itahari"
+
+Sarangs=Employee()
+Sarangs.address
+Sarangs.company
+
