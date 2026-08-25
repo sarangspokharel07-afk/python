6b982e7c112a698d90ce0b61f299190d03f4f27f
sarangspokharel07-afk <sarangspokharel07@gmail.com>
2026-08-25 15:29:45 +0545

    form

---
+++ form.py
@@ -0,0 +1,14 @@
+#WAP to create a object name form and add all the details as a __init__() constructor print all the details
+
+class Form:
+    def __init__(self, name, age, email, phone):
+        self.name = name
+        self.age = age
+        self.email = email
+        self.phone = phone
+
+sarangs = Form("Sarangs", 20, "sarangs@example.com", "1234567890")
+
+print(sarangs.name, sarangs.age, sarangs.email, sarangs.phone)
+
+
