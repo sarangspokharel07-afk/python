4c00498400b63bdfa1230129796ad5c4fcec0acb
sarangspokharel07-afk <sarangspokharel07@gmail.com>
2026-08-31 15:33:17 +0545

    multiple inheritance

--- multiple.py
+++ multiple.py
@@ -1,3 +1,9 @@
+class Grandfather:
+
+    def skills(self):
+        print("Gardening")
+
+
 class Father:
 
     def skills1(self):
@@ -8,12 +14,13 @@ class Mother:
     def skills2(self):
         print("Cooking")        
     
-class Child(Father, Mother):
+class Child(Father, Mother, Grandfather):
 
     def skills3(self):
         print("Coding")
 
 c=Child()
+c.skills()
 c.skills1()
 c.skills2()
 c.skills3()
